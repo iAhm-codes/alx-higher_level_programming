7bca3faaef50a5b15497b0b5f6387dbea19cec33
iAhm-codes <omegabliss23@gmail.com>
2023-06-14 21:01:07 +0100

    Code: added code to print the modlus of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
 
-last_digit = number % 10
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = -last_digit
+
+print(f"last digit of {number} is {last_digit} and is ", end="")
 
 if (last_digit > 5):
-    print(f"last digit of {number} is {last_digit} and is greater than 5")
+    print(f"greater than 5")
 
 elif (last_digit == 0):
-    print(f"last digit of {number} is {last_digit} and is 0")
-elif (last_digit < 6 and last_digit != 0):
-    print(f"last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"0")
+
+else:
+    print(f"less than 6 and not 0")
